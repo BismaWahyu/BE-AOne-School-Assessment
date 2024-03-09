2bdda0d18dfab340baee6f1d183f2aff9cc85c9c
BismaWahyu <bw.anaafie@gmail.com>
2024-03-09 12:14:25 +0700

    vercel config

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
+
+// if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+//     // Handle GET request
+//     echo json_encode(['message' => 'Hello from /api/index.php']);
+// } else {
+//     // Handle other HTTP methods
+//     http_response_code(405); // Method Not Allowed
+//     echo json_encode(['error' => 'Method not allowed']);
+// }
